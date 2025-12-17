2320467ca000491b0007f9a9ce06d8f7d97a8646
PVK7773 <vk.patil@icloud.com>
2025-12-17 11:43:16 +0530

    Set default user session state values

--- pages/1_Dashboard.py
+++ pages/1_Dashboard.py
@@ -1,9 +1,10 @@
 import streamlit as st
 from components.sidebar import load_sidebar
 
+# Load sidebar
 load_sidebar()
 
-user = st.session_state.get("user")
+user = st.session_state.get("user", {"name": "Employee", "role": "Staff"})
 
 st.title("Welcome to the Microland HR Portal")
 st.write(f"Hello, **{user['name']}** ({user['role']})")
@@ -12,9 +13,12 @@ col1, col2, col3, col4 = st.columns(4)
 
 with col1:
     st.markdown("<div class='tile'><div class='tile-number'>2</div><div class='tile-label'>My Documents</div></div>", unsafe_allow_html=True)
+
 with col2:
     st.markdown("<div class='tile'><div class='tile-number'>8</div><div class='tile-label'>Leave Balance</div></div>", unsafe_allow_html=True)
+
 with col3:
     st.markdown("<div class='tile'><div class='tile-number'>1</div><div class='tile-label'>Leave Requests</div></div>", unsafe_allow_html=True)
+
 with col4:
     st.markdown("<div class='tile'><div class='tile-number'>0</div><div class='tile-label'>Pending Approvals</div></div>", unsafe_allow_html=True)
