91f4c1c27cb952f6fcee3aa610315d4b77e14504
PVK7773 <vk.patil@icloud.com>
2025-12-17 11:28:12 +0530

    Refactor leave application options and button text

--- pages/3_Leave_Applications.py
+++ pages/3_Leave_Applications.py
@@ -1,10 +1,13 @@
 import streamlit as st
+from streamlit_app import sidebar
+
+sidebar()
 
 st.title("Leave Applications")
 
-leave_type = st.selectbox("Leave Type", ["Casual Leave","Sick Leave","Annual Leave"])
+leave_type = st.selectbox("Leave Type", ["Casual", "Sick", "Annual"])
 days = st.number_input("Days", 1, 30)
 reason = st.text_area("Reason")
 
-if st.button("Apply Leave"):
+if st.button("Submit Leave Request"):
     st.success("Leave request submitted!")
