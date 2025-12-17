a9a067f87dc082d50ef5729775789c786a76e75a
PVK7773 <vk.patil@icloud.com>
2025-12-17 11:28:37 +0530

    Update employee data and add sidebar

--- pages/4_Employee_Directory.py
+++ pages/4_Employee_Directory.py
@@ -1,12 +1,15 @@
 import streamlit as st
+from streamlit_app import sidebar
 import pandas as pd
+
+sidebar()
 
 st.title("Employee Directory")
 
 df = pd.DataFrame({
-    "Name":["Alister Dsouza","John Paul","Sanjay Kumar"],
-    "Role":["Manager","Engineer","Finance"],
-    "Email":["alister.dsouza@microland.com","john@microland.com","sanjay@microland.com"]
+    "Name": ["Alister Noel Dsouza", "John Peter", "Rohit Kumar"],
+    "Email": ["alister.dsouza@microland.com", "john.p@microland.com", "rohit@microland.com"],
+    "Department": ["IT", "Operations", "Finance"]
 })
 
 st.dataframe(df)
